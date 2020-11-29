cbdc57bf7a46929e860bda66084b554f635722c9
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-28 22:47:17 -0500

    update main server file with middlewarers and new router

--- VideoTools/app.js
+++ VideoTools/app.js
@@ -1,10 +1,17 @@
 
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 const videoToolRouter = require('./videoTools');
 
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
+//routers
 app.use(videoToolRouter);
 
+//establish connection to local host
 app.listen(4437);
